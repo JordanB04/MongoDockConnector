4b6215f2fdf888948dcc0636603e20b75012d1ff
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-09-25 15:12:32 -0400

    test: success case using Testcontainers Mongo verifies PingAsync returns true

--- MongoDockConnector.Tests/MongoDBConnector.Tests.cs
+++ MongoDockConnector.Tests/MongoDBConnector.Tests.cs
@@ -1,10 +1,36 @@
-﻿namespace MongoDockConnector.Tests;
+﻿using System.Threading.Tasks;
+using MongoDockConnector.Lib;
+using Testcontainers.MongoDb;
+using Xunit;
 
-public class UnitTest1
+namespace MongoDockConnector.Tests
 {
-    [Fact]
-    public void Test1()
+    public class MongoDBConnector_SuccessTest : IAsyncLifetime
     {
+        private MongoDbContainer _mongo;
 
+        public async Task InitializeAsync()
+        {
+            _mongo = new MongoDbBuilder()
+                .WithImage("mongo:7")
+                .Build();
+            await _mongo.StartAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_mongo is not null)
+            {
+                await _mongo.DisposeAsync();
+            }
+        }
+
+        [Fact]
+        public async Task PingAsync_ReturnsTrue_WhenMongoIsReachable()
+        {
+            var connector = new MongoDBConnector(_mongo.GetConnectionString());
+            var ok = await connector.PingAsync();
+            Assert.True(ok);
+        }
     }
 }
